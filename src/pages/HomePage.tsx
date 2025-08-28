b3d5ad415eddfd14529f3fd44a69b69aafe1ad6a
bobbythree <bobbythr33@gmail.com>
2025-08-28 15:23:54 -0500

    adjust homepage gradient colors

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,7 +4,7 @@ import bgImg from '../assets/ivy_cat.jpg'
 export default function HomePage() {
   return (
 
-    <div className="relative flex items-center justify-center h-screen bg-gradient-to-tr from-green-500 to-yellow-200">
+    <div className="relative flex items-center justify-center h-screen bg-gradient-to-tr from-green-900 via-green-600 to-yellow-200">
       <img
         src={bgImg}
         alt=""
