2d2cd4ea1859e8d6004e5f7507ed015ddd87fd77
bobbythree <bobbythr33@gmail.com>
2025-08-28 14:15:48 -0500

    transparent color gradient bg

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,13 +3,17 @@ import bgImg from '../assets/ivy_cat.jpg'
 
 export default function HomePage() {
   return (
-    <div className="h-screen bg-cover bg-gradient-to-tr from-green-500 to-yellow-200"
-      style={{
-        backgroundImage: `url(${bgImg})`,
-        backgroundPosition: 'center 30%',
-      }}>
 
+    <div className="relative h-screen bg-gradient-to-tr from-green-600 to-yellow-200">
+      <img
+        src={bgImg}
+        alt=""
+        className="absolute inset-0 w-full h-full object-cover opacity-20"
+
+      />
     </div>
+
+
   )
 }
 
