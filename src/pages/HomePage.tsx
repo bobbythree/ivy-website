1c36957d90b6ded2449e74359585865f0c89c04b
bobbythree <bobbythr33@gmail.com>
2025-08-28 15:10:36 -0500

    adjust homepage bg color

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,16 +1,14 @@
 import HomeHero from "../components/HomeHero"
-import { NavLink } from "react-router"
 import bgImg from '../assets/ivy_cat.jpg'
 
 export default function HomePage() {
   return (
 
-    <div className="relative flex items-center justify-center h-screen bg-gradient-to-tr from-green-600 to-yellow-200">
+    <div className="relative flex items-center justify-center h-screen bg-gradient-to-tr from-green-500 to-yellow-200">
       <img
         src={bgImg}
         alt=""
-        className="absolute inset-0 w-full h-full object-cover opacity-20"
-
+        className="absolute inset-0 w-full h-full object-cover opacity-15"
       />
       <HomeHero />
     </div>
