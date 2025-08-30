cfd30f3d1645805ae10ea150157c90b172cb5c80
bobbythree <bobbythr33@gmail.com>
2025-08-30 11:04:13 -0500

    add absolute to burger menu

--- src/components/BurgerMenu.tsx
+++ src/components/BurgerMenu.tsx
@@ -10,13 +10,13 @@ export default function BurgerMenu() {
   }
 
   return (
-    <div className="font-londrina-solid-light flex flex-col p-5">
+    <div className="font-londrina-solid-light flex flex-col p-5 relative">
       <button onClick={handleClick} className="w-8">
         <img src={burger} className="w-8 h-8" />
       </button>
       {
         isOpen && (
-          <div className="w-1/12 text-2xl flex flex-col gap-8 pt-8">
+          <div className="w-1/12 text-2xl flex flex-col gap-8 pt-8 absolute top-15">
             <NavLink to='/' className="hover:text-yellow-200">home</NavLink>
             <NavLink to='/about' className="hover:text-yellow-200">about</NavLink>
             <NavLink to='/art' className="hover:text-yellow-200">art</NavLink>
